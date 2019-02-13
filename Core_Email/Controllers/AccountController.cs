c40e67ca3802e2da861ad1e038b5d87c6d1fcad2
Sous Chili <greatdragone75@gmail.com>
2019-02-14 03:00:44 +0400

    add log in view

---
+++ Core_Email/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Core_Email.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Email.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Enter()
+        {
+            DataInputViewModel model = new DataInputViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Enter(DataInputViewModel model)
+        {
+            //todo add validation on server side 
+            return Content($"This is post method {model.Login}--{model.Password}--{model.Title}");
+        }
+
+
+    }
+}
---
+++ Core_Email/ViewModels/DataInputViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Email.ViewModels
+{
+    public class DataInputViewModel
+    {
+
+        //тест стринг удалить!
+        public string Title { get; set; } = "This is test string";
+        [Required(ErrorMessage = "Input Login")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Input password")]
+        public string Password { get; set; }
+
+    }
+}
